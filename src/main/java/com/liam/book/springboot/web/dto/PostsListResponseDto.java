82f82b9a8b67f19313b9afd2e0e2ec299e4551c0
HanHyeonggyu <1hanho94@gmail.com>
2022-01-15 20:08:12 +0900

    git ignore 등록

---
+++ src/main/java/com/liam/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,22 @@
+package com.liam.book.springboot.web.dto;
+
+import com.liam.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedTime;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.author = entity.getAuthor();
+        this.title = entity.getTitle();
+        this.modifiedTime = entity.getModifiedDate();
+    }
+
+}
