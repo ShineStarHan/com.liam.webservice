5b30cf2583b4a76455f8acf3bcc8d36240b713bd
HanHyeonggyu <1hanho94@gmail.com>
2022-01-18 23:51:02 +0900

    AWS배포전까지

--- src/main/java/com/liam/book/springboot/web/IndexController.java
+++ src/main/java/com/liam/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.liam.book.springboot.web;
 
+import com.liam.book.springboot.config.auth.LoginUser;
+import com.liam.book.springboot.config.auth.dto.SessionUser;
 import com.liam.book.springboot.service.posts.PostsService;
 import com.liam.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,14 +11,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if(user != null){
+            System.out.println("================="+user.getName());
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
 
     }
